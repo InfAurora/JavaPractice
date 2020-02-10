4c11721487bca9e382746e2cbb97dc18575c56ce
Jose Alba <albajo0123@gmail.com>
2020-02-10 14:17:38 -0600

    Added service layer to classRoster and audit page

--- NetBeansProjects/ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
+++ NetBeansProjects/ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
@@ -5,9 +5,11 @@
  */
 package com.sg.classroster.controller;
 
-import com.sg.classroster.dao.ClassRosterDao;
-import com.sg.classroster.dao.ClassRosterDaoException;
+import com.sg.classroster.dao.ClassRosterPersistenceException;
 import com.sg.classroster.dto.Student;
+import com.sg.classroster.service.ClassRosterDataValidationException;
+import com.sg.classroster.service.ClassRosterDuplicateIdException;
+import com.sg.classroster.service.ClassRosterServiceLayer;
 import com.sg.classroster.ui.ClassRosterView;
 import java.util.List;
 
@@ -17,13 +19,13 @@ import java.util.List;
  */
 public class ClassRosterController {
 
-    public ClassRosterController(ClassRosterDao dao, ClassRosterView view) {
-        this.dao = dao;
+    public ClassRosterController(ClassRosterServiceLayer service, ClassRosterView view) {
+        this.service = service;
         this.view = view;
     }
 
     private ClassRosterView view;
-    private ClassRosterDao dao;
+    private ClassRosterServiceLayer service;
     //private UserIO io = new UserIOConsoleImpl();
 
     public void run() {
@@ -56,7 +58,7 @@ public class ClassRosterController {
 
             }
             exitMessage();
-        } catch (ClassRosterDaoException e) {
+        } catch (ClassRosterPersistenceException e) {
             view.displayErrorMessage(e.getMessage());
         }
     }
@@ -65,30 +67,41 @@ public class ClassRosterController {
         return view.printMenuAndGetSelection();
     }
 
-    private void createStudent() throws ClassRosterDaoException {
+    private void createStudent() throws ClassRosterPersistenceException {
         view.displayCreateStudentBanner();
-        Student newStudent = view.getNewStudentInfo();
-        dao.addStudent(newStudent.getStudentId(), newStudent);
-        view.displayCreateSuccessBanner();
+        boolean hasErrors = false;
+        do {
+            Student newStudent = view.getNewStudentInfo();
+
+         try {
+            service.createStudent(newStudent);
+            view.displayCreateSuccessBanner();
+            hasErrors = false;
+        } catch (ClassRosterDuplicateIdException | ClassRosterDataValidationException e) {
+            hasErrors = true;
+            view.displayErrorMessage(e.getMessage());
+        }
+    }while(hasErrors);
+        
     }
 
-    private void listStudents() throws ClassRosterDaoException {
+    private void listStudents() throws ClassRosterPersistenceException {
         view.displayDisplayAllBanner();
-        List<Student> studentList = dao.getAllStudents();
+        List<Student> studentList = service.getAllStudents();
         view.displayStudentList(studentList);
     }
 
-    private void viewStudent() throws ClassRosterDaoException {
+    private void viewStudent() throws ClassRosterPersistenceException {
         view.displayDisplayStudentBanner();
         String studentId = view.getStudentIdChoice();
-        Student student = dao.getStudent(studentId);
+        Student student = service.getStudent(studentId);
         view.displayStudent(student);
     }
 
-    private void removeStudent() throws ClassRosterDaoException {
+    private void removeStudent() throws ClassRosterPersistenceException {
         view.displayRemoveStudentBanner();
         String studentId = view.getStudentIdChoice();
-        Student removedStudent = dao.removeStudent(studentId);
+        Student removedStudent = service.removeStudent(studentId);
         view.displayRemoveResult(removedStudent);
     }
 
--- NetBeansProjects/ClassRoster/src/main/java/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
+++ NetBeansProjects/ClassRoster/src/main/java/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
@@ -31,7 +31,7 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
 
     @Override
     public Student addStudent(String studentId, Student student)
-            throws ClassRosterDaoException {
+            throws ClassRosterPersistenceException {
         loadRoster();
         Student newStudent = students.put(studentId, student);
         writeRoster();
@@ -40,21 +40,21 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
 
     @Override
     public List<Student> getAllStudents()
-            throws ClassRosterDaoException {
+            throws ClassRosterPersistenceException {
         loadRoster();
         return new ArrayList(students.values());
     }
 
     @Override
     public Student getStudent(String studentId)
-            throws ClassRosterDaoException {
+            throws ClassRosterPersistenceException {
         loadRoster();
         return students.get(studentId);
     }
 
     @Override
     public Student removeStudent(String studentId)
-            throws ClassRosterDaoException {
+            throws ClassRosterPersistenceException {
         loadRoster();
         Student removedStudent = students.remove(studentId);
         writeRoster();
@@ -99,7 +99,7 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
         return studentFromFile;
     }
 
-    private void loadRoster() throws ClassRosterDaoException {
+    private void loadRoster() throws ClassRosterPersistenceException {
         Scanner scanner;
 
         try {
@@ -108,7 +108,7 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
                     new BufferedReader(
                             new FileReader(ROSTER_FILE)));
         } catch (FileNotFoundException e) {
-            throw new ClassRosterDaoException(
+            throw new ClassRosterPersistenceException(
                     "-_- Could not load roster data into memory.", e);
         }
         // currentLine holds the most recent line read from the file
@@ -160,9 +160,9 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
      * Writes all students in the roster out to a ROSTER_FILE. See loadRoster
      * for file format.
      *
-     * @throws ClassRosterDaoException if an error occurs writing to the file
+     * @throws ClassRosterPersistenceException if an error occurs writing to the file
      */
-    private void writeRoster() throws ClassRosterDaoException {
+    private void writeRoster() throws ClassRosterPersistenceException {
         // NOTE FOR APPRENTICES: We are not handling the IOException - but
         // we are translating it to an application specific exception and 
         // then simple throwing it (i.e. 'reporting' it) to the code that
@@ -173,7 +173,7 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
         try {
             out = new PrintWriter(new FileWriter(ROSTER_FILE));
         } catch (IOException e) {
-            throw new ClassRosterDaoException(
+            throw new ClassRosterPersistenceException(
                     "Could not save student data.", e);
         }
 
